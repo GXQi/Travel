dd20a9526ee3145b936716f0cdb22f52b7f74ffd
GXQi <15809237114@163.com>
2018-11-26 17:24:52 +0800

    finish city search

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,51 @@
 <template>
-<div class="search">
-  <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+<div>
+  <div class="search">
+    <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
+  </div>
+  <div class="search-content">
+    <ul>
+      <li v-for="item of list" :key="item.id">{{ item.name }}</li>
+    </ul>
+  </div>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  props: {
+    cities: Object
+  },
+  wacth: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  }
 }
 </script>
 
@@ -25,4 +64,13 @@ export default {
     box-sizing: border-box
     border-radius: .06rem
     color: #666
+.search-content
+  z-index: 1
+  overflow: hidden
+  position: absolute
+  top: 1.78rem
+  bottom: 0
+  left: 0
+  right: 0
+  background: red
 </style>
