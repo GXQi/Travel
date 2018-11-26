1295707436adcfd449bc59f83fa7ec6ca7af435d
GXQi <15809237114@163.com>
2018-11-26 16:10:13 +0800

    performance

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -17,7 +17,11 @@
       </div>
       </div>
     </div>
-    <div class="area" v-for="(item, key) of cities" :key="key">
+    <div class="area"
+      v-for="(item, key) of cities"
+      :key="key"
+      :ref="key"
+    >
       <div class="title border-topbottom">{{ key }}</div>
       <div class="item-list">
         <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{ innerItem.name }}</div>
@@ -32,8 +36,17 @@ import BetterScroll from 'better-scroll'
 export default {
   name: 'CityList',
   props: {
+    hotCities: Array,
     cities: Object,
-    hotCities: Array
+    letter: String
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   },
   mounted () {
     this.scroll = new BetterScroll(this.$refs.wrapper)
